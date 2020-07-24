0796bc926280f04693b82a5b839ee13ff3d33225
Barry Deering <barry.deering@gmail.com>
2020-07-24 12:39:10 +0100

    Latest HLT - 24/07

--- resources.tf
+++ resources.tf
@@ -31,20 +31,16 @@ resource "azurerm_subnet" "sn_udemy_subnet1" {
     name                          = "TestConfiguartion1"
     subnet_id                     = azurerm_subnet.sn_udemy_subnet1.id
     private_ip_address_allocation = "Dynamic"
-    #public_ip_address_id = azurerm_public_ip.pip_udemy_web_server.id
-     #load_balancer_inbound_nat_rules_ids = [azurerm_lb_nat_rule.nat_udemy_webserver.id]
   }
 }
 
 resource "azurerm_public_ip" "pip_udemy_web_server" {
-  name                = "${var.prefix}-${var.udemy_web_server}-${format("%02d",count.index)}-pip"
+  name                = "${var.prefix}-${var.udemy_web_server}-pip"
   location            = azurerm_resource_group.rg_udemy.location
   resource_group_name = azurerm_resource_group.rg_udemy.name
-  #condional example (true or false) - if production then Static else Dynamic
   allocation_method   = "Static"
-  domain_name_label = "${var.prefix}-${var.udemy_web_server}-${format("%02d",count.index)}-dns"  
-  count               = var.udemy_web_server_count
-
+  domain_name_label =  "${var.prefix}-${var.udemy_web_server}-dns"
+  #count               = var.udemy_web_server_count
 }
 
 resource "azurerm_network_security_group" "sn_udemy_subnet1_nsg" {
@@ -97,54 +93,56 @@ resource "azurerm_windows_virtual_machine" "vm_udemy_vm1" {
 }
 
 resource "azurerm_lb" "lb_udemy_webserver" {
-  name                = "${var.prefix}-${var.udemy_web_server}-${format("%02d",count.index)}-lb"
+  name                = "${var.prefix}-${var.udemy_web_server}-lb"
   location            = azurerm_resource_group.rg_udemy.location
   resource_group_name = azurerm_resource_group.rg_udemy.name
-  count               = var.udemy_web_server_count
+  #count               = var.udemy_web_server_count
 
   frontend_ip_configuration {
     name                          = "LoadBalancerFrontEnd"
-    public_ip_address_id          = azurerm_public_ip.pip_udemy_web_server[count.index].id
+    public_ip_address_id          = azurerm_public_ip.pip_udemy_web_server.id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "lbbe_udemy_webserver" {
     resource_group_name = azurerm_resource_group.rg_udemy.name
-    loadbalancer_id     = azurerm_lb.lb_udemy_webserver[count.index].id
+    loadbalancer_id     = azurerm_lb.lb_udemy_webserver.id
     name                = "BackEndAddressPool"
-    count = 2
+    #count = 2
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "lbbepool_udemy_webserver" {
-    network_interface_id    = azurerm_network_interface.nic_udemy_web_server[count.index].id
+    #network_interface_id    =   count.index == 0 ? azurerm_network_interface.nic_udemy_web_server.id : null
+    network_interface_id    =   azurerm_network_interface.nic_udemy_web_server[0].id
     #ip_configuration_name   = "ipConfiguration"
     ip_configuration_name    = "TestConfiguartion1"
-    backend_address_pool_id = azurerm_lb_backend_address_pool.lbbe_udemy_webserver[count.index].id 
-    count = 2  
+    backend_address_pool_id = azurerm_lb_backend_address_pool.lbbe_udemy_webserver.id 
+    #count = 2  
 }
 
 resource "azurerm_lb_nat_rule" "nat_udemy_webserver" {
   resource_group_name = azurerm_resource_group.rg_udemy.name
-  loadbalancer_id                = azurerm_lb.lb_udemy_webserver[count.index].id
+  loadbalancer_id                = azurerm_lb.lb_udemy_webserver.id
   name                           = "RDPNAT"
   protocol                       = "Tcp"
   frontend_port                  = 443
   backend_port                   = 3389
   frontend_ip_configuration_name = "LoadBalancerFrontEnd"
-  count = 2
+  #count = 2
 }
 
 resource "azurerm_network_interface_nat_rule_association" "natasc_udemy_webserver" {
-  network_interface_id  = azurerm_network_interface.nic_udemy_web_server[count.index].id
+  #network_interface_id  = count.index == 0 ? azurerm_network_interface.nic_udemy_web_server.id : null
+  network_interface_id  = azurerm_network_interface.nic_udemy_web_server[0].id
   ip_configuration_name = "TestConfiguartion1"
-  nat_rule_id           = azurerm_lb_nat_rule.nat_udemy_webserver[count.index].id
-  count = 2
+  nat_rule_id           = azurerm_lb_nat_rule.nat_udemy_webserver.id
+  #count = 2
 }
 
-#resource "azurerm_availability_set" "as_udemy_webserver" {
- #   name                = "${var.prefix}-${var.udemy_web_server}-as"
- #   location            = azurerm_resource_group.rg_udemy.location
- #   resource_group_name = azurerm_resource_group.rg_udemy.name
- #   platform_fault_domain_count = "2"
+resource "azurerm_availability_set" "as_udemy_webserver" {
+    name                = "${var.prefix}-${var.udemy_web_server}-as"
+    location            = azurerm_resource_group.rg_udemy.location
+    resource_group_name = azurerm_resource_group.rg_udemy.name
+    platform_fault_domain_count = "2"
 
-#}
+}
